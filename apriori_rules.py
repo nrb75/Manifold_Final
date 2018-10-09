ea706787fe910ed0dd30613519c360154bbe1c24
Natalie <nrb75@cornell.edu>
2018-10-09 08:32:02 -0400

    apriori rules added

---
+++ apriori_rules.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  4 16:35:48 2018
+
+@author: natalie
+"""
+
+from iter_apriori import freq
+from iter_apriori import order_count
+from iter_apriori import get_item_pairs
+from iter_apriori import merge_item_stats
+from iter_apriori import merge_item_name
+from itertools import combinations, groupby
+from collections import Counter
+
+def association_rules(order_item, min_support, min_confidence):
+
+    print("Starting order_item: {:22d}".format(len(order_item)))
+
+
+    # Calculate item frequency and support
+    item_stats             = freq(order_item).to_frame("freq")
+    item_stats['support']  = item_stats['freq'] / order_count(order_item) * 100
+
+
+    # Filter from order_item items below min support 
+    qualifying_items       = item_stats[item_stats['support'] >= min_support].index
+    order_item             = order_item[order_item.isin(qualifying_items)]
+
+    print("Items with support >= {}: {:15d}".format(min_support, len(qualifying_items)))
+    print("Remaining order_item: {:21d}".format(len(order_item)))
+
+
+    # Filter from order_item orders with less than 2 items
+    order_size             = freq(order_item.index)
+    qualifying_orders      = order_size[order_size >= 2].index
+    order_item             = order_item[order_item.index.isin(qualifying_orders)]
+
+    print("Remaining orders with 2+ items: {:11d}".format(len(qualifying_orders)))
+    print("Remaining order_item: {:21d}".format(len(order_item)))
+
+
+    # Recalculate item frequency and support
+    item_stats             = freq(order_item).to_frame("freq")
+    item_stats['support']  = item_stats['freq'] / order_count(order_item) * 100
+
+
+    # Get item pairs generator
+    item_pair_gen          = get_item_pairs(order_item)
+
+
+    # Calculate item pair frequency and support
+    item_pairs              = freq(item_pair_gen).to_frame("freqAB")
+    item_pairs['supportAB'] = item_pairs['freqAB'] / len(qualifying_orders) * 100
+
+    print("Item pairs: {:31d}".format(len(item_pairs)))
+
+
+    # Filter from item_pairs those below min support
+    item_pairs              = item_pairs[item_pairs['supportAB'] >= min_support]
+
+    print("Item pairs with support >= {}: {:10d}\n".format(min_support, len(item_pairs)))
+
+
+    # Create table of association rules and compute relevant metrics
+    item_pairs = item_pairs.reset_index().rename(columns={'level_0': 'item_A', 'level_1': 'item_B'})
+    item_pairs = merge_item_stats(item_pairs, item_stats)
+    
+    item_pairs['confidenceAtoB'] = item_pairs['supportAB'] / item_pairs['supportA']
+    item_pairs['confidenceBtoA'] = item_pairs['supportAB'] / item_pairs['supportB']
+    item_pairs['lift']           = item_pairs['supportAB'] / (item_pairs['supportA'] * item_pairs['supportB'])
+    
+    #filter rules that are below minimum confidence set by user
+    
+    item_pairs                   = item_pairs[item_pairs['confidenceAtoB'] >= min_confidence]
+    # Return association rules sorted by lift in descending order
+    return item_pairs.sort_values('lift', ascending=False)
